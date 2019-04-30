940745f1e2afcc613f2f064f2a7ea29674b5c391
Eric Lopez <lpzericl@gmail.com>
2019-04-30 13:49:00 -0500

    changed working of valid/invalid to active/inactive

--- src/components/Roles.js
+++ src/components/Roles.js
@@ -5,7 +5,7 @@ const Roles = ({name, type, description, active}) => {
 
     return (
         <div>
-            { active === 'yes' ? <Badge variant="success">Valid</Badge> : <Badge variant="danger">Invalid</Badge> }
+            { active === 'yes' ? <Badge variant="success">Active</Badge> : <Badge variant="danger">Inactive</Badge> }
             <h2>{ name }</h2>
             <h3>{ type }</h3>
             <p>{ description }</p>
